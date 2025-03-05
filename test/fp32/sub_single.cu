13ac958680c6d7e60fd7e3056392289cc2c49b49
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2025-03-04 20:38:43 -0700

    Sub tests

--- test/fp32/sub_single.cu
+++ test/fp32/sub_single.cu
@@ -5,89 +5,89 @@
 
 // @min_sm 20
 
-// add{.rnd}{.ftz}{.sat}.f32  d, a, b;
+// sub{.rnd}{.ftz}{.sat}.f32  d, a, b;
 
 // .rnd = { .rn, .rz, .rm, .rp };
 
 __global__ void test___(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test___sat(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test__ftz_(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.ftz.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.ftz.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test__ftz_sat(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.ftz.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.ftz.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test_rn__(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.rn.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.rn.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test_rn__sat(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.rn.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.rn.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test_rn_ftz_(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.rn.ftz.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.rn.ftz.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test_rn_ftz_sat(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.rn.ftz.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.rn.ftz.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test_rz__(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.rz.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.rz.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test_rz__sat(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.rz.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.rz.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test_rz_ftz_(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.rz.ftz.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.rz.ftz.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test_rz_ftz_sat(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.rz.ftz.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.rz.ftz.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test_rm__(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.rm.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.rm.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test_rm__sat(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.rm.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.rm.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test_rm_ftz_(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.rm.ftz.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.rm.ftz.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test_rm_ftz_sat(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.rm.ftz.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.rm.ftz.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test_rp__(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.rp.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.rp.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test_rp__sat(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.rp.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.rp.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test_rp_ftz_(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.rp.ftz.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.rp.ftz.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 __global__ void test_rp_ftz_sat(const float* a, const float* b, float* c) {
     int idx = threadIdx.x;
-    asm ("add.rp.ftz.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
+    asm ("sub.rp.ftz.sat.f32 %0, %1, %2;" : "=f"(c[idx]) : "f"(a[idx]), "f"(b[idx]));
 }
 
 typedef void (*kernel_t)(const float*, const float*, float*);
@@ -98,14 +98,14 @@ int main() {
                           test_rn__sat, test_rn_ftz_, test_rn_ftz_sat, test_rz__,
                           test_rz__sat, test_rz_ftz_, test_rz_ftz_sat, test_rm__,
                           test_rm__sat, test_rm_ftz_, test_rm_ftz_sat, test_rp__,
-                          test_rp__sat, test_rp_ftz_, test_rp_ftz_sat};
+                          test_rp__sat, test_rp_ftz_, test_rp_ftz_sat};  
     cudaMallocManaged(&A, warpSize * sizeof(float));
     cudaMallocManaged(&B, warpSize * sizeof(float));
     cudaMallocManaged(&C, warpSize * sizeof(float));
     std::pair<float, float> error_pairs[] = {
-        {INFINITY, -INFINITY},
-        {FLT_MAX, FLT_MAX},
-        {FLT_MIN, -FLT_MIN/2}};
+        {INFINITY, INFINITY},
+        {FLT_MAX, -FLT_MAX},
+        {FLT_MIN, FLT_MIN/2}};
     for (kernel_t k : kernels) {
         for (auto [a, b] : error_pairs) {
             fill_array_float(A, warpSize, a);
