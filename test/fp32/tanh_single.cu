ef316bc97e86fe493abdfb1ed2adbe5c40d9a57b
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2025-03-05 11:22:28 -0700

    fix tanh

--- test/fp32/tanh_single.cu
+++ test/fp32/tanh_single.cu
@@ -5,20 +5,16 @@
 
 // @min_sm 20
 
-__global__ void test_ex2_approx_(const float* a, float* b) {
+__global__ void test_tanh_approx_(const float* a, float* b) {
     int idx = threadIdx.x;
-    asm ("ex2.approx.f32 %0, %1;" : "=f"(b[idx]) : "f"(a[idx]));
-}
-__global__ void test_ex2_approx_ftz(const float* a, float* b) {
-    int idx = threadIdx.x;
-    asm ("ex2.approx.ftz.f32 %0, %1;" : "=f"(b[idx]) : "f"(a[idx]));
+    asm ("tanh.approx.f32 %0, %1;" : "=f"(b[idx]) : "f"(a[idx]));
 }
 
 typedef void (*kernel_t)(const float*, float*);
 int main() {
     int warpSize = 32;
     float *A, *B;
-    kernel_t kernels[] = {test_ex2_approx_, test_ex2_approx_ftz};
+    kernel_t kernels[] = {test_tanh_approx_};
     cudaMallocManaged(&A, warpSize * sizeof(float));
     cudaMallocManaged(&B, warpSize * sizeof(float));
     float vals[] = {-1.0, FLT_MIN, 0.0, -0.0, INFINITY, NAN};
@@ -27,6 +23,9 @@ int main() {
             fill_array_float(A, warpSize, val);
             k<<<1, 32>>>(A, B);
             cudaDeviceSynchronize();
+            for (int i = 0; i < warpSize; i++) {
+                printf("tanh.approx(%e) = %e\n", A[i], B[i]);
+            }
         }
     }
     cudaFree(A);
