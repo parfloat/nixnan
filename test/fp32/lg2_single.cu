c19e67621dc6c3f7cc96b4e884389cb8849e9b2d
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2025-03-05 11:17:52 -0700

    lg2

---
+++ test/fp32/lg2_single.cu
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <cuda_runtime.h>
+#include "helpers.cuh"
+#include <cfloat>
+
+// @min_sm 20
+
+__global__ void test_lg2_approx_(const float* a, float* b) {
+    int idx = threadIdx.x;
+    asm ("lg2.approx.f32 %0, %1;" : "=f"(b[idx]) : "f"(a[idx]));
+}
+__global__ void test_lg2_approx_ftz(const float* a, float* b) {
+    int idx = threadIdx.x;
+    asm ("lg2.approx.ftz.f32 %0, %1;" : "=f"(b[idx]) : "f"(a[idx]));
+}
+
+typedef void (*kernel_t)(const float*, float*);
+int main() {
+    int warpSize = 32;
+    float *A, *B;
+    kernel_t kernels[] = {test_lg2_approx_, test_lg2_approx_ftz};
+    cudaMallocManaged(&A, warpSize * sizeof(float));
+    cudaMallocManaged(&B, warpSize * sizeof(float));
+    float vals[] = {-1.0, FLT_MIN, 0.0, -0.0, INFINITY};
+    for (kernel_t k : kernels) {
+        for (float val : vals) {
+            fill_array_float(A, warpSize, val);
+            k<<<1, 32>>>(A, B);
+            cudaDeviceSynchronize();
+        }
+    }
+    cudaFree(A);
+    cudaFree(B);
+    return 0;
+}
